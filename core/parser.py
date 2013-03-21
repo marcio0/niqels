d8e84666f1db0db84a52f2201f9bd9ed493e395b
Marcio <marcio.cruz@ispm.com.br>
2013-03-20 22:01:49 -0300

    finishing value catching and some of date parsing

--- core/parser.py
+++ core/parser.py
@@ -1,10 +1,34 @@
 import re
 from decimal import Decimal
+import datetime
+
+
+def get_date(s):
+    pattern = r'\d(\d)?/\d(\d)?(/(?P<Y>\d\d\d\d)|/(?P<y>\d\d))?'
+    value = re.search(pattern, s)
+    if value:
+        date_str = value.group(0)
+
+        if value.group('Y'):
+            date_pattern = r'%d/%m/%Y'
+        elif value.group('y'):
+            date_pattern = r'%d/%m/%y'
+        else:
+            date_str = r'%s/%d' % (date_str, datetime.date.today().year)
+            date_pattern = r'%d/%m/%Y'
+
+        date = datetime.datetime.strptime(date_str, date_pattern).date()
+
+        return date
+    return None
 
 
 def get_value(s):
-    pattern = r'\d+(.\d+)?'
+    pattern = r'[+-]?\d+(.\d+)?'
     value = re.search(pattern, s)
     if value:
-        return Decimal(value.group(0))
+        value = value.group(0)
+        if value[0] not in '+-':
+            value = '-%s' % value
+        return Decimal(value)
     return None
--- core/tests.py
+++ core/tests.py
@@ -1,20 +1,60 @@
 from django.test import TestCase
-from core.parser import parse_entry, get_value, get_date
+from core.parser import get_value, get_date
 from core.models import Entry, Category
 from decimal import Decimal
 import datetime
 
 
-class TypeTest(TestCase):
-    def test_get_value(self):
-        self.assertIsInstance(get_value('valor 50'), Decimal)
+class DateParsingTest(TestCase):
+    def test_type(self):
+        self.assertIsInstance(get_date('01/01'), datetime.date)
 
-        self.assertEquals(get_value('valor 50'), Decimal(50))
-        self.assertEquals(get_value('valor 50.'), Decimal(50))
-        self.assertEquals(get_value('valor 50.0'), Decimal(50))
-        self.assertEquals(get_value('valor 50.10'), Decimal('50.10'))
-        self.assertEquals(get_value('valor 0.10'), Decimal('.10'))
-        #self.assertEquals(get_value('valor .10'), Decimal('.10')) # TODO
+    def test_month_day(self):
+        # If year is absent, default is actual year.
+        expected = datetime.date(month=01, day=01, year=datetime.date.today().year)
+        self.assertEquals(get_date('date 01/01'), expected)
+        self.assertEquals(get_date('date 1/01'), expected)
+        self.assertEquals(get_date('date 01/1'), expected)
+
+    def test_month_day_full_year(self):
+        expected = datetime.date(month=01, day=01, year=2001)
+        self.assertEquals(get_date('date 01/01/2001'), expected)
+        self.assertEquals(get_date('date 1/01/2001'), expected)
+        self.assertEquals(get_date('date 01/1/2001'), expected)
+
+    def test_month_day_abbr_year(self):
+        expected = datetime.date(month=01, day=01, year=2001)
+        self.assertEquals(get_date('date 01/01/01'), expected)
+        self.assertEquals(get_date('date 1/01/01'), expected)
+        self.assertEquals(get_date('date 01/1/01'), expected)
+
+
+class ValueParsingTest(TestCase):
+    def test_type(self):
+        self.assertIsInstance(get_value('1'), Decimal)
+
+    def test_get_value(self):
+        # Default is negative.
+        self.assertEquals(get_value('valor 50'), Decimal(-50))
+        self.assertEquals(get_value('valor 50.'), Decimal(-50))
+        self.assertEquals(get_value('valor 50.0'), Decimal(-50))
+        self.assertEquals(get_value('valor 50.10'), Decimal('-50.10'))
+        self.assertEquals(get_value('valor 0.10'), Decimal('-.10'))
+        #self.assertEquals(get_value('valor .10'), Decimal('-.10')) # TODO
+
+    def test_get_negative_value(self):
+        self.assertEquals(get_value('valor -50'), Decimal(-50))
+        self.assertEquals(get_value('valor -50.'), Decimal(-50))
+        self.assertEquals(get_value('valor -50.0'), Decimal(-50))
+        self.assertEquals(get_value('valor -50.10'), Decimal('-50.10'))
+        self.assertEquals(get_value('valor -0.10'), Decimal('-.10'))
+
+    def test_get_positive_value(self):
+        self.assertEquals(get_value('valor +50'), Decimal(50))
+        self.assertEquals(get_value('valor +50.'), Decimal(50))
+        self.assertEquals(get_value('valor +50.0'), Decimal(50))
+        self.assertEquals(get_value('valor +50.10'), Decimal('50.10'))
+        self.assertEquals(get_value('valor +0.10'), Decimal('.10'))
 
     def test_get_value_failure(self):
         self.assertEquals(get_value('no value'), None)
