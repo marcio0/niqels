fca08013d39b70a4c86ca77db6fd1ccb70c3340c
Marcio <marcio.cruz@ispm.com.br>
2013-03-24 00:30:59 -0300

    turning regex parsing into a class

--- core/parser.py
+++ core/parser.py
@@ -4,78 +4,78 @@ from core.models import Category
 import datetime
 
 
-def parse_expense(s):
-    category = get_category(s)
-    value = get_value(s)
-    date = get_date(s)
+class ExpenseRegexParser(object):
+    def __init__(self, string):
+        self.string = string
 
-    if not date:
-        date = datetime.date.today()
+    def parse_expense(self):
+        category = self.get_category()
+        value = self.get_value()
+        date = self.get_date()
 
-    return dict(
-        category=category,
-        value=value,
-        date=date
-    )
-    
+        if not date:
+            date = datetime.date.today()
 
-def get_description(s):
-    pattern = ''.join([
-        r'(?<=["\'])', # Starts with ' or ".
-        r'(.*)' # Anything after that.
-    ])
-    value = re.search(pattern, s)
+        return dict(
+            category=category,
+            value=value,
+            date=date
+        )
 
-    if value:
-        return value.group(0)
-    return None
+    def get_description(self):
+        pattern = ''.join([
+            r'(?<=["\'])', # Starts with ' or ".
+            r'(.*)' # Anything after that.
+        ])
+        value = re.search(pattern, self.string)
 
+        if value:
+            return value.group(0)
+        return None
 
-def get_category(s):
-    pattern = r'[a-zA-Z]+' # A word.
-    value = re.search(pattern, s)
+    def get_category(self):
+        pattern = r'[a-zA-Z]+' # A word.
+        value = re.search(pattern, self.string)
 
-    if(value):
-        return value.group(0)
-    return None
+        if(value):
+            return value.group(0)
+        return None
 
+    def get_date(self):
+        pattern = ''.join([
+            r'\d(\d)?/\d(\d)?', # Month and day.
+            r'(/(?P<Y>\d\d\d\d)|/(?P<y>\d\d))?' # Year is optional. Might be 4 or 2 chars long.
+        ])
+        value = re.search(pattern, self.string)
 
-def get_date(s):
-    pattern = ''.join([
-        r'\d(\d)?/\d(\d)?', # Month and day.
-        r'(/(?P<Y>\d\d\d\d)|/(?P<y>\d\d))?' # Year is optional. Might be 4 or 2 chars long.
-    ])
-    value = re.search(pattern, s)
+        if value:
+            date_str = value.group(0)
 
-    if value:
-        date_str = value.group(0)
+            if value.group('Y'):
+                date_pattern = r'%d/%m/%Y'
+            elif value.group('y'):
+                date_pattern = r'%d/%m/%y'
+            else:
+                date_str = r'%s/%d' % (date_str, datetime.date.today().year)
+                date_pattern = r'%d/%m/%Y'
 
-        if value.group('Y'):
-            date_pattern = r'%d/%m/%Y'
-        elif value.group('y'):
-            date_pattern = r'%d/%m/%y'
-        else:
-            date_str = r'%s/%d' % (date_str, datetime.date.today().year)
-            date_pattern = r'%d/%m/%Y'
+            date = datetime.datetime.strptime(date_str, date_pattern).date()
 
-        date = datetime.datetime.strptime(date_str, date_pattern).date()
+            return date
+        return None
 
-        return date
-    return None
+    def get_value(self):
+        # TODO: Fix conflict with date if it comes first.
+        pattern = ''.join([
+            r'[+-]?', # Might be declared as positive or negative.
+            r'\d+', # Any number.
+            r'(\.\d+)?' # Decimal values are optional.
+        ])
+        value = re.search(pattern, self.string)
 
-
-def get_value(s):
-    # TODO: Fix conflict with date if it comes first.
-    pattern = ''.join([
-        r'[+-]?', # Might be declared as positive or negative.
-        r'\d+', # Any number.
-        r'(\.\d+)?' # Decimal values are optional.
-    ])
-    value = re.search(pattern, s)
-
-    if value:
-        value = value.group(0)
-        if value[0] not in '+-':
-            value = '-%s' % value # A negative value is default.
-        return Decimal(value)
-    return None
+        if value:
+            value = value.group(0)
+            if value[0] not in '+-':
+                value = '-%s' % value # A negative value is default.
+            return Decimal(value)
+        return None
--- core/tests/parser.py
+++ core/tests/parser.py
@@ -1,14 +1,17 @@
 from django.test import TestCase
-from core.parser import get_value, get_date, get_category, get_description, parse_expense
+from core.parser import ExpenseRegexParser
 from core.models import Category
 from decimal import Decimal
 import datetime
 
+class ExpenseParsingTest(object):
 
-class ExpenseParsingTest(TestCase):
+    # Date parsing:
+
     def test_default_date(self):
         s = 'Light 50'
-        expense = parse_expense(s)
+        parser = self.parser(s)
+        expense = parser.parse_expense()
 
         self.assertEquals(expense['value'], Decimal('-50'))
         self.assertEquals(expense['category'], 'Light')
@@ -18,109 +21,163 @@ class ExpenseParsingTest(TestCase):
         date = datetime.date(2010, 06, 10)
 
         s = "40 10/06/2010"
-        expense = parse_expense(s)
+        parser = self.parser(s)
+        expense = parser.parse_expense()
 
         self.assertEquals(expense['value'], Decimal('-40'))
         self.assertEquals(expense['date'], date)
 
         s = "10/06/2010 40"
-        expense = parse_expense(s)
+        parser = self.parser(s)
+        expense = parser.parse_expense()
 
         self.assertEquals(expense['value'], Decimal('-40'))
         self.assertEquals(expense['date'], date)
 
-
-
-class DescriptionParsingTest(TestCase):
+    # Description parsing:
     # For now, description must be at the end.
     # It starts with quotes ('), double quotes (") or sharp (#).
-    def test_type(self):
+    def test_description_type(self):
         s = 'cat 40 "lots of stuff'
-        self.assertIsInstance(get_description(s), str)
+        parser = self.parser(s)
+        
+        self.assertIsInstance(parser.get_description(), str)
 
     def test_get_description(self):
         # Double quotes.
         s = 'cat 40 "lots of stuff'
-        self.assertEquals(get_description(s), 'lots of stuff')
+        parser = self.parser(s)
+        self.assertEquals(parser.get_description(), 'lots of stuff')
 
         # Single quotes.
         s = "cat 40 'lots of stuff"
-        self.assertEquals(get_description(s), 'lots of stuff')
+        parser = self.parser(s)
+        self.assertEquals(parser.get_description(), 'lots of stuff')
 
         # Special characters.
         s = "cat 40 'lots of stuff 34 $$ .;,~"
-        self.assertEquals(get_description(s), 'lots of stuff 34 $$ .;,~')
+        parser = self.parser(s)
+        self.assertEquals(parser.get_description(), 'lots of stuff 34 $$ .;,~')
 
+    # Category parsing:
 
-class CategoryParsingTest(TestCase):
-    def test_type(self):
-        self.assertIsInstance(get_category('category 50 10/01'), str)
+    def test_category_type(self):
+        s = 'category 50 10/01'
+        parser = self.parser(s)
+        self.assertIsInstance(parser.get_category(), str)
 
     def test_get_category(self):
-        cat = get_category('example 50 10/01')
+        s = 'example 50 10/01'
+        parser = self.parser(s)
+        cat = parser.get_category()
         self.assertEquals(cat, 'example')
 
-        cat = get_category('50 example 10/01')
+        s = '50 example 10/01'
+        parser = self.parser(s)
+        cat = parser.get_category()
         self.assertEquals(cat, 'example')
 
     def test_capitalization(self):
-        cat = get_category('50 EXAMPle 10/01')
+        s = '50 EXAMPle 10/01'
+        parser = self.parser(s)
+        cat = parser.get_category()
         self.assertEquals(cat, 'EXAMPle')
 
+    # Date parsing:
 
-class DateParsingTest(TestCase):
-    def test_type(self):
-        self.assertIsInstance(get_date('01/01'), datetime.date)
+    def test_date_type(self):
+        s = '01/01'
+        parser = self.parser(s)
+        self.assertIsInstance(parser.get_date(), datetime.date)
 
     def test_month_day(self):
         # If year is absent, default is actual year.
         expected = datetime.date(month=01, day=01, year=datetime.date.today().year)
-        self.assertEquals(get_date('date 01/01'), expected)
-        self.assertEquals(get_date('date 1/01'), expected)
-        self.assertEquals(get_date('date 01/1'), expected)
+
+        parser = self.parser('date 01/01')
+        self.assertEquals(parser.get_date(), expected)
+
+        parser = self.parser('date 1/01')
+        self.assertEquals(parser.get_date(), expected)
+
+        parser = self.parser('date 01/1')
+        self.assertEquals(parser.get_date(), expected)
 
     def test_month_day_full_year(self):
         expected = datetime.date(month=01, day=01, year=2001)
-        self.assertEquals(get_date('date 01/01/2001'), expected)
-        self.assertEquals(get_date('date 1/01/2001'), expected)
-        self.assertEquals(get_date('date 01/1/2001'), expected)
+
+        parser = self.parser('date 01/01/2001')
+        self.assertEquals(parser.get_date(), expected)
+
+        parser = self.parser('date 1/01/2001')
+        self.assertEquals(parser.get_date(), expected)
+
+        parser = self.parser('date 01/1/2001')
+        self.assertEquals(parser.get_date(), expected)
 
     def test_month_day_abbr_year(self):
         expected = datetime.date(month=01, day=01, year=2001)
-        self.assertEquals(get_date('date 01/01/01'), expected)
-        self.assertEquals(get_date('date 1/01/01'), expected)
-        self.assertEquals(get_date('date 01/1/01'), expected)
 
+        parser = self.parser('date 01/01/01')
+        self.assertEquals(parser.get_date(), expected)
 
-class ValueParsingTest(TestCase):
-    def test_type(self):
-        self.assertIsInstance(get_value('1'), Decimal)
+        parser = self.parser('date 1/01/01')
+        self.assertEquals(parser.get_date(), expected)
+
+        parser = self.parser('date 01/1/01')
+        self.assertEquals(parser.get_date(), expected)
+
+    # Value parsing:
+
+    def test_value_type(self):
+        parser = self.parser('1')
+        self.assertIsInstance(parser.get_value(), Decimal)
 
     def test_get_value(self):
         # Default is negative.
-        self.assertEquals(get_value('valor 50'), Decimal(-50))
-        self.assertEquals(get_value('valor 50.'), Decimal(-50))
-        self.assertEquals(get_value('valor 50.0'), Decimal(-50))
-        self.assertEquals(get_value('valor 50.10'), Decimal('-50.10'))
-        self.assertEquals(get_value('valor 0.10'), Decimal('-.10'))
+        parser = self.parser('valor 50')
+        self.assertEquals(parser.get_value(), Decimal(-50))
+        parser = self.parser('valor 50.')
+        self.assertEquals(parser.get_value(), Decimal(-50))
+        parser = self.parser('valor 50.0')
+        self.assertEquals(parser.get_value(), Decimal(-50))
+        parser = self.parser('valor 50.10')
+        self.assertEquals(parser.get_value(), Decimal('-50.10'))
+        parser = self.parser('valor 0.10')
+        self.assertEquals(parser.get_value(), Decimal('-.10'))
         #self.assertEquals(get_value('valor .10'), Decimal('-.10')) # TODO
 
     def test_get_negative_value(self):
-        self.assertEquals(get_value('valor -50'), Decimal(-50))
-        self.assertEquals(get_value('valor -50.'), Decimal(-50))
-        self.assertEquals(get_value('valor -50.0'), Decimal(-50))
-        self.assertEquals(get_value('valor -50.10'), Decimal('-50.10'))
-        self.assertEquals(get_value('valor -0.10'), Decimal('-.10'))
+        parser = self.parser('valor -50')
+        self.assertEquals(parser.get_value(), Decimal(-50))
+        parser = self.parser('valor -50.')
+        self.assertEquals(parser.get_value(), Decimal(-50))
+        parser = self.parser('valor -50.0')
+        self.assertEquals(parser.get_value(), Decimal(-50))
+        parser = self.parser('valor -50.10')
+        self.assertEquals(parser.get_value(), Decimal('-50.10'))
+        parser = self.parser('valor -0.10')
+        self.assertEquals(parser.get_value(), Decimal('-.10'))
 
     def test_get_positive_value(self):
-        self.assertEquals(get_value('valor +50'), Decimal(50))
-        self.assertEquals(get_value('valor +50.'), Decimal(50))
-        self.assertEquals(get_value('valor +50.0'), Decimal(50))
-        self.assertEquals(get_value('valor +50.10'), Decimal('50.10'))
-        self.assertEquals(get_value('valor +0.10'), Decimal('.10'))
+        parser = self.parser('valor +50')
+        self.assertEquals(parser.get_value(), Decimal(50))
+        parser = self.parser('valor +50.')
+        self.assertEquals(parser.get_value(), Decimal(50))
+        parser = self.parser('valor +50.0')
+        self.assertEquals(parser.get_value(), Decimal(50))
+        parser = self.parser('valor +50.10')
+        self.assertEquals(parser.get_value(), Decimal('50.10'))
+        parser = self.parser('valor +0.10')
+        self.assertEquals(parser.get_value(), Decimal('.10'))
 
     def test_get_value_failure(self):
-        self.assertEquals(get_value('no value'), None)
+        parser = self.parser('no value')
+        self.assertEquals(parser.get_value(), None)
+
+
+class ExpenseRegexParsingTest(ExpenseParsingTest, TestCase):
+    parser = ExpenseRegexParser
 
 
 class CategoryModelTest(TestCase):
