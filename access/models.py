8b883187b063dac664f9e6335b666398da2b2d4b
Marcio <marcio.cruz@ispm.com.br>
2013-04-10 04:32:57 +0000

    adding date_joined to user

--- access/models.py
+++ access/models.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
@@ -12,6 +13,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
 
     objects = UserManager()
 
