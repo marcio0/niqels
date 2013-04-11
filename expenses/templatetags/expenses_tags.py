f9c101384fa17c653ee413f12ca1a8b738f4eb4f
Marcio <marcio.cruz@ispm.com.br>
2013-04-11 23:53:07 +0000

    showing categories as bootstrap labels

--- expenses/templatetags/expenses_tags.py
+++ expenses/templatetags/expenses_tags.py
@@ -1,6 +1,12 @@
 from django import template
+from django.utils.safestring import mark_safe
+
 
 register = template.Library()
 
+
+@register.filter(is_safe=True)
 def as_category(value):
-    return '<span class="label">%s</span>' % value
+    # TODO: check for auto escaping:
+    # https://docs.djangoproject.com/en/dev/howto/custom-template-tags/#filters-and-auto-escaping
+    return mark_safe('<span class="label">%s</span>' % value)
