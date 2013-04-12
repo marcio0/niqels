f7324157a36b9906340cfbf3296954411598e4af
Marcio <marcio.cruz@ispm.com.br>
2013-04-12 03:52:20 +0000

    oops

--- access/views.py
+++ access/views.py
@@ -1,3 +1,6 @@
+from django.views.generic.base import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.shortcuts import render, redirect
 from django.contrib import messages
--- expenses/views.py
+++ expenses/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic.base import TemplateView, View
+from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 from django.http import HttpResponseRedirect
 
