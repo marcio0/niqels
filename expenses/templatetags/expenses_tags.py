bc2909642e6b8c39af7f9886967d6da93c2be988
Marcio <marcio.cruz@ispm.com.br>
2013-04-13 21:59:41 +0000

    coloring entry values

--- expenses/templatetags/expenses_tags.py
+++ expenses/templatetags/expenses_tags.py
@@ -11,3 +11,18 @@ def as_category(value):
     # TODO: check for auto escaping:
     # https://docs.djangoproject.com/en/dev/howto/custom-template-tags/#filters-and-auto-escaping
     return mark_safe(label_html_tag % (value.color, value.name.capitalize()))
+
+
+@register.filter(is_safe=True)
+def as_value(value):
+    # TODO: usar a moeda do locale
+    label_html_tag = '<span class="text-%s">$ %d</span>'
+    
+    if value < 0:
+        color = 'error'
+    else:
+        color = 'success'
+
+    # TODO: check for auto escaping:
+    # https://docs.djangoproject.com/en/dev/howto/custom-template-tags/#filters-and-auto-escaping
+    return mark_safe(label_html_tag % (color, value))
