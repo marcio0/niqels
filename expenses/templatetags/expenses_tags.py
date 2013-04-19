a261255b58f67ed11da10ff7797ddb13b4ad9f04
Marcio <marcio.cruz@ispm.com.br>
2013-04-19 03:49:29 +0000

    tweaking entry-list layout

--- expenses/templatetags/expenses_tags.py
+++ expenses/templatetags/expenses_tags.py
@@ -10,7 +10,7 @@ register = template.Library()
 
 @register.filter(is_safe=True)
 def as_category(value):
-    label_html_tag = '<span class="label" style="background-color: %s;">%s</span>'
+    label_html_tag = '<span class="category-label" style="background-color: %s;">%s</span>'
     # TODO: check for auto escaping:
     # https://docs.djangoproject.com/en/dev/howto/custom-template-tags/#filters-and-auto-escaping
     return mark_safe(label_html_tag % (value.color, value.name.capitalize()))
@@ -19,7 +19,7 @@ def as_category(value):
 @register.filter(is_safe=True)
 def as_value(value):
     # TODO: usar a moeda do locale
-    label_html_tag = '<span class="text-%s">$ %d</span>'
+    label_html_tag = '<span class="text-%s">$%d</span>'
     
     if value < 0:
         color = 'error'
