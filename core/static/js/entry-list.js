583798f76af5e0abe7b56122be17129866c2449f
Marcio <marcio.cruz@ispm.com.br>
2013-04-20 03:23:02 +0000

    coloring value input text color based on input

--- core/static/js/entry-list.js
+++ core/static/js/entry-list.js
@@ -1,6 +1,5 @@
 $(function(){
     var $dateField = $('#id_date');
-        
 
     $dateField.datepicker({
         format: 'mm/dd/yyyy'
@@ -8,5 +7,26 @@ $(function(){
 
     $dateField.focusout(function() {
         $(this).data('datepicker').hide();
+    });
+
+    var $valueField = $('#id_value');
+
+    $valueField.keyup(function(event) {
+        var me = $(this),
+            color = "";
+
+        if (me.val().charAt(0) === '-') {
+            color = '#b94a48';
+        }
+        else if (parseInt(me.val().charAt(0))) {
+            color = '#b94a48';
+        }
+        else if (me.val().charAt(0) === '+') {
+            color = '#468847';
+        }
+        else {
+            return;
+        }
+        me.css('color', color);
     });
 });
