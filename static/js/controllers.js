49afaf3a609d20674d7e4c560960b7978dab8afb
Marcio <marcio.cruz@ispm.com.br>
2013-05-24 04:20:10 +0000

    finishing transaction form

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,11 +1,11 @@
-function TransactionFormCtrl ($scope, $element, $http, $location, Transaction) {
+function TransactionFormCtrl ($scope, $element, $http, $location, Transaction, Category) {
     $scope.transaction = {};
     $scope.errors = {};
 
     $http.defaults.headers.post['X-CSRFToken'] = $('input[name=csrfmiddlewaretoken]', $element).val();
 
     $scope.submit = function () {
-        var transaction = $scope.transaction,
+        var transaction_data = $scope.transaction,
             form = this.entryform;
 
         if (form.$invalid) {
@@ -14,16 +14,25 @@ function TransactionFormCtrl ($scope, $element, $http, $location, Transaction) {
             $scope.errors.date = form.date.$error.required;
         }
         else {
+            // clears invalid state
             $scope.errors = {};
-            Transaction.save(transaction, function(){
-                $location.url('/#/transactions');
+
+            Transaction.save(transaction_data).$then(function (value) {
+                // getting the category info
+                var cat = Category.cacheLookup(transaction_data.category),
+                    transaction = value.resource;
+                transaction.category = cat;
+
+                // $scope.transactions.objects.push(transaction);
+
+                return transaction;
             });
         }
     };
 }
 
 function TransactionListCtrl($scope, Transaction) {
-    $scope.transactions = Transaction.query();
+    $scope.transactions = Transaction.get();
 
     indexed = [];
 
@@ -38,8 +47,7 @@ function TransactionListCtrl($scope, Transaction) {
             indexed.push(transaction.date);
         }
         return isNew;
-    }
-
+    };
 }
 
 function TransactionDetailCtrl($scope, $routeParams, Transaction) {
--- static/js/services.js
+++ static/js/services.js
@@ -1,9 +1,52 @@
 angular.module('transactionService', ['ngResource']).
-    factory('Transaction', function($resource, $route){
-        return $resource('/api/v1/transaction/:id', {}, {
-            query: {
+    factory('Transaction', function($resource, $cacheFactory){
+        var Transaction = $resource('/api/v1/transaction/:id', {}, {
+            get: {
                 method: 'GET',
                 isArray: false
             }
         });
+
+        return Transaction;
+    });
+
+angular.module('categoryService', ['ngResource']).
+    factory('Category', function($resource, $cacheFactory){
+
+        var cache = $cacheFactory('Category');
+        var Category = $resource('/api/v1/category/:id', {}, {
+            get: {
+                method: 'GET',
+                isArray: false
+            }
+        });
+
+        // caching all categories
+        Category.get().$then(function (value) {
+            var objs = value.data.objects;
+
+            for (var i in objs) {
+                var obj = objs[i];
+                cache.put(obj.name, obj);
+            }
+            return value;
+        });
+
+        Category.cacheLookup = function (catName) {
+            var category = cache.get(catName);
+
+            // if this category is not present on the cache, retrieve from the api and saves it
+            if (category) {
+                return category;
+            }
+            else {
+                return Category.get({name: catName}).$then(function (data) {
+                    var category = data.data.objects[0];
+                    cache.put(category.name, category);
+                    return data;
+                });
+            }
+        };
+
+        return Category;
     });
