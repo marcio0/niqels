d6685b4984bccff7c0c15e148686cbd8f19ffc4d
Marcio <marcio.cruz@ispm.com.br>
2013-07-18 05:12:25 +0000

    fixing migration

--- expenses/migrations/0002_auto__add_field_entry_repeatable.py
+++ expenses/migrations/0002_auto__add_field_entry_repeatable.py
@@ -6,6 +6,9 @@ from django.db import models
 
 
 class Migration(SchemaMigration):
+    depends_on = (
+        ("reminder", "0001_initial"),
+    )
 
     def forwards(self, orm):
         # Adding field 'Entry.repeatable'
