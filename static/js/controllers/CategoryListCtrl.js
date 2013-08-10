a2f6ff1dc1467b49ad081bca8c069b0e2ee9d82c
Marcio <marcio.cruz@ispm.com.br>
2013-08-10 21:04:48 +0000

    showing modal on click on category row

--- static/js/controllers/CategoryListCtrl.js
+++ static/js/controllers/CategoryListCtrl.js
@@ -1,8 +1,20 @@
-function CategoryListCtrl ($scope, Category) {
+'use strict';
+
+function CategoryListCtrl ($scope, Category, $modal, $q) {
     Category.query().$then(function (result) {
         var categories = result.resource;
         $scope.categories = categories;
     });
+
+    $scope.editCategory = function editCategory (category) {
+        var scope = $scope.$new();
+        scope.category = category;
+
+        var modalPromise = $modal({template: '/partials/category-edit/', persist: "false", show: false, backdrop: 'static', scope: scope});
+        $q.when(modalPromise).then(function(modalEl) {
+            modalEl.modal('show');
+        });
+    };
 }
 
-CategoryListCtrl.$inject = ['$scope', 'Category'];
+CategoryListCtrl.$inject = ['$scope', 'Category', '$modal', '$q'];
