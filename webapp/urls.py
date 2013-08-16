85e72d1da30e4e9ab8fef3497f6d9978ec1ff71d
Marcio <marcio.cruz@ispm.com.br>
2013-08-16 23:06:47 +0000

    refactoring settings templates

--- webapp/urls.py
+++ webapp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^$', WebAppView.as_view(), name="index"),
     url(r'^partials/transaction-list/$', TemplateView.as_view(template_name="webapp/transaction-list.html")),
 
-    url(r'^partials/settings/$', TemplateView.as_view(template_name="webapp/settings.html")),
+    url(r'^partials/settings/$', TemplateView.as_view(template_name="webapp/settings/settings.html")),
     url(r'^partials/category-list/$', TemplateView.as_view(template_name="webapp/settings/category-list.html")),
     url(r'^partials/category-edit/$', TemplateView.as_view(template_name="webapp/settings/category-edit.html")),
     url(r'^partials/reminder-list/$', TemplateView.as_view(template_name="webapp/settings/reminder-list.html")),
