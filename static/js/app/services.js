06af8bfcfcbd557eec992f5e38e6d4a180ad42db
Marcio <marcio.cruz@ispm.com.br>
2013-08-19 04:28:30 +0000

    cleaning useless code

--- static/js/app/services.js
+++ static/js/app/services.js
@@ -121,35 +121,6 @@ angular.module('models', ['ngResource'])
             update: {method: 'PUT'}
         });
 
-        var loadCache = function (value) {
-            var objs = value.data.objects;
-
-            for (var i in objs) {
-                var obj = objs[i];
-                cache.put(obj.name, obj);
-            }
-            return value;
-        };
-
-        // caching all categories
-        Category.get().$then(loadCache);
-
-        Category.cacheLookup = function (catName) {
-            var category = cache.get(catName);
-
-            // if this category is not present on the cache, retrieve from the api and saves it
-            if (category) {
-                return category;
-            }
-            else {
-                return Category.get({name: catName}).$then(function (data) {
-                    var category = data.data.objects[0];
-                    cache.put(category.name, category);
-                    return data;
-                });
-            }
-        };
-
         $rootScope.$on('categoryUpdated', function (e, value, opts) {
             opts = opts || {};
             if (opts && !opts.silent) {
