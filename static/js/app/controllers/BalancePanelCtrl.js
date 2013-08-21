6d2ecbb7ab9610c44ae350ffc680c1120241f1e1
Marcio <marcio.cruz@ispm.com.br>
2013-08-21 02:07:34 +0000

    adding highcharts & directive

--- static/js/app/controllers/BalancePanelCtrl.js
+++ static/js/app/controllers/BalancePanelCtrl.js
@@ -20,6 +20,48 @@ function BalancePanelCtrl ($scope, $http, $rootScope, $filter) {
         });
     };
 
+    var chartOptions = {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+            categories: [
+                'Jan',
+                'Feb',
+                'Mar'
+            ]
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: ''
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Balanço',
+            data: [1, 2, 3]
+        }]
+    };
+
+    $rootScope.chartData = chartOptions;
+
     $rootScope.$on('transactionCreated', $scope.updateBalance);
     $rootScope.$on('transactionRemoved', $scope.updateBalance);
     $rootScope.$watch('month', $scope.updateBalance);
