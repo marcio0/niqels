e3b6ff46c750dabd5ec0963ad8e7aba6fa719039
Marcio <marcio.cruz@ispm.com.br>
2013-09-26 03:45:50 +0000

    tweaking admin

--- expenses/admin.py
+++ expenses/admin.py
@@ -3,15 +3,16 @@ from django.contrib import admin
 from expenses.models import Category, Transaction, CategoryGroup
 
 class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = Category._meta.get_all_field_names()
+    search_fields = ['name']
+    actions = None
+    list_display = ('name', 'group')
+    list_filter = ('default_active', 'custom', 'group')
+
     def has_add_permission(self, request, obj=None):
         return False
     def has_delete_permission(self, request, obj=None):
         return False
-
-    readonly_fields = Category._meta.get_all_field_names()
-
-    search_fields = ['name']
-
 admin.site.register(Category, CategoryAdmin)
 
 
@@ -21,9 +22,9 @@ class CategoryGroupAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
-    readonly_fields = CategoryGroup._meta.get_all_field_names()
-
+    readonly_fields = ['name']
     search_fields = ['name']
+    actions = None
 
 admin.site.register(CategoryGroup, CategoryGroupAdmin)
 
