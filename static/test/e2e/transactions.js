0c2c285baa33755be0cc5e4dd830707dfb3c17f5
Marcio <marcio.cruz@ispm.com.br>
2013-10-06 03:27:40 +0000

    testing positive transactions

--- static/test/e2e/transactions.js
+++ static/test/e2e/transactions.js
@@ -1,15 +1,6 @@
 'use strict';
 
-function type (value) {
-    return function (elements, done) {
-        for (var i in value.split('')) {
-            $(elements).sendkeys(value[i]);
-        }
-        done();
-    }
-}
-
-describe('transactions', function () {
+describe('transactions section', function () {
 
     beforeEach(function() {
         browser().navigateTo('/test_login?email=existing@test.com');
@@ -18,25 +9,48 @@ describe('transactions', function () {
     describe('transaction creation', function () {
         var form = 'form.transaction-form ';
 
-        it('should create a transaction', function () {
+        it('creating a negative transaction', function () {
             // creating a transaction
             element(form + 'button[data-id="id_category"]').click();
             element(form + 'div.bootstrap-select > div.dropdown-menu li[rel=1] > a').click();
             input('formData.value').enter('-100');
-            input('formData.description').enter('a transaction');
+            input('formData.description').enter('a negative transaction');
             element(form + 'button.btn-primary').click();
 
             expect(element('.toast.toast-success').count()).toBeTruthy();
 
             expect(element('table.transaction-table tr.transaction-row').count()).toBe(1);
 
-            expect(element('table.transaction-table tr.transaction-row td.value-column span').text()).toBe('- R$100,00');
-            expect(element('table.transaction-table tr.transaction-row td.category-column').text()).toBe('Mercado');
-            expect(element('table.transaction-table tr.transaction-row td.description-column').text()).toBe('a transaction');
+            var row = 'table.transaction-table tbody[rel=0] tr.transaction-row[rel=0] ';
+            expect(element(row + 'td.value-column span').text()).toBe('- R$100,00');
+            expect(element(row + 'td.category-column').text()).toBe('Mercado');
+            expect(element(row + 'td.description-column').text()).toBe('a negative transaction');
 
             expect(element('div#balance-panel .balance-info .renevues > .value').text()).toBe('R$0,00');
             expect(element('div#balance-panel .balance-info .expenses > .value').text()).toBe('- R$100,00');
             expect(element('div#balance-panel .balance-info .total > .value').text()).toBe('- R$100,00');
+        });
+
+        it('creating a positive transaction', function () {
+            // creating a transaction
+            element(form + 'button[data-id="id_category"]').click();
+            element(form + 'div.bootstrap-select > div.dropdown-menu li[rel=1] > a').click();
+            input('formData.value').enter('+200');
+            input('formData.description').enter('a positive transaction');
+            element(form + 'button.btn-primary').click();
+
+            expect(element('.toast.toast-success').count()).toBeTruthy();
+
+            expect(element('table.transaction-table tr.transaction-row').count()).toBe(2);
+
+            var row = 'table.transaction-table tbody[rel=0] tr.transaction-row[rel=0] ';
+            expect(element(row + 'td.value-column span').text()).toBe('R$200,00');
+            expect(element(row + 'td.category-column').text()).toBe('Mercado');
+            expect(element(row + 'td.description-column').text()).toBe('a positive transaction');
+
+            expect(element('div#balance-panel .balance-info .renevues > .value').text()).toBe('R$200,00');
+            expect(element('div#balance-panel .balance-info .expenses > .value').text()).toBe('- R$100,00');
+            expect(element('div#balance-panel .balance-info .total > .value').text()).toBe('R$100,00');
         });
     });
 });
