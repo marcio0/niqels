498691e59fa7cf002a94caf16e7ae8872c3e4eee
Marcio <marcio.cruz@ispm.com.br>
2013-10-24 02:54:14 +0000

    cleanup

--- api/resources/category_resource.py
+++ api/resources/category_resource.py
@@ -38,22 +38,3 @@ class CategoryResource(ModelResource):
 
     def dehydrate_group(self, bundle):
         return bundle.data['group'].data['name']
-
-    def _obj_create(self, bundle, **kwargs):
-        """
-        Creating a inactive category will reactivate it.
-        """
-        try:
-            bundle.obj = self._meta.object_class.objects.get(name=bundle.data.get('name'))
-        except self._meta.object_class.DoesNotExist:
-            bundle.obj = self._meta.object_class()
-
-        bundle.obj.active = True
-        bundle.obj.user = bundle.request.user
-
-        for key, value in kwargs.items():
-            setattr(bundle.obj, key, value)
-
-        self.authorized_create_detail(self.get_object_list(bundle.request), bundle)
-        bundle = self.full_hydrate(bundle)
-        return self.save(bundle)
