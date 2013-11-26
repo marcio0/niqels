c3a9aa0c5904524aa32d07346bfeb584d7170e84
Marcio <marcio.cruz@ispm.com.br>
2013-11-26 22:50:22 +0000

    removing balancechart

--- static/src/js/app/controllers/balance-panel-ctrl.js
+++ static/src/js/app/controllers/balance-panel-ctrl.js
@@ -1,4 +1,4 @@
-function BalancePanelCtrl ($scope, $http, $rootScope, $filter, calculators, BalanceChart) {
+function BalancePanelCtrl ($scope, $rootScope, BalanceChart) {
     'use strict';
 
     $scope.updateBalance = function () {
@@ -6,9 +6,8 @@ function BalancePanelCtrl ($scope, $http, $rootScope, $filter, calculators, Bala
 
         var reference_date, date_start, date_end;
 
-        date_end = $rootScope.filterDate;
-        date_start = date_end.clone().subtract(2, 'months');
-
+        date_end = $rootScope.filterDate.endOf('month');
+        date_start = $rootScope.filterDate.startOf('month');
         var params = {
             date_start: date_start.format('YYYY-MM-DD'),
             date_end: date_end.format('YYYY-MM-DD')
@@ -16,27 +15,18 @@ function BalancePanelCtrl ($scope, $http, $rootScope, $filter, calculators, Bala
 
         var data = BalanceChart.fetchData(params).then(function setupScope (result) {
             var httpResult = result.result,
-                this_month = httpResult.data[httpResult.data.length -1];
+                this_month = httpResult.data[0];
 
             $scope.renevues = parseFloat(this_month.renevues);
             $scope.expenses = parseFloat(this_month.expenses);
 
             return result;
         });
-        if ($rootScope.device != 'phone') {
-            // don't create the chart if it's on a mobile
-            $scope.chartData = data;
-        }
     };
 
     $rootScope.$on('transaction-created', $scope.updateBalance);
     $rootScope.$on('transaction-removed', $scope.updateBalance);
     $rootScope.$watch('filterDate', $scope.updateBalance);
-    $rootScope.$on('devicechanged', function (e, device) {
-        if (device != 'phone') {
-            $scope.updateBalance();
-        }
-    });
 }
 
-BalancePanelCtrl.$inject = ['$scope', '$http', '$rootScope', '$filter', 'calculators', 'BalanceChart'];
+BalancePanelCtrl.$inject = ['$scope', '$rootScope', 'BalanceChart'];
