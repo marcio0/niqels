862ddcf68c8d8b2732f102ea7577d338bb493a72
Marcio <marcio.cruz@ispm.com.br>
2013-12-11 01:17:44 +0000

    started working on installments

---
+++ static/src/js/app/controllers/installment-form-ctrl.js
@@ -0,0 +1,11 @@
+function InstallmentFormCtrl ($scope) {
+    "use strict";
+
+    $scope.is_installment = false;
+
+    $scope.selectInstallment = function selectInstallment () {
+
+    };
+}
+
+InstallmentFormCtrl.$inject = ['$scope'];
