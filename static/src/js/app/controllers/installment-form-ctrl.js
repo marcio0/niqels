00fbac4e8f239827c35aa6bcf47292a43a6faf0f
Marcio <marcio.cruz@ispm.com.br>
2013-12-13 00:49:03 +0000

    form control

--- static/src/js/app/controllers/installment-form-ctrl.js
+++ static/src/js/app/controllers/installment-form-ctrl.js
@@ -1,9 +1,31 @@
-function InstallmentFormCtrl ($scope) {
+function InstallmentFormCtrl ($scope, $filter) {
     "use strict";
+
+    $scope.installment = {
+        qtyInstallments: 1
+    };
 
     $scope.selectInstallment = function selectInstallment () {
 
     };
+
+    $scope.updateQtyInstallments = function () {
+        if ($scope.installment.totalValue) {
+            var totalValue = parseFloat($scope.installment.totalValue.replace(',', '.'), 2);
+            var installmentValue = (totalValue / $scope.installment.qtyInstallments).toFixed(2);
+            $scope.installment.installmentValue = installmentValue;
+        }
+    };
+
+    $scope.updateInstallmentValue = function () {
+        var installmentValue = parseFloat($scope.installment.installmentValue.replace(',', '.'), 2);
+        $scope.installment.totalValue = (installmentValue * $scope.installment.qtyInstallments).toFixed(2).toString();
+    };
+
+    $scope.updateTotalValue = function () {
+        var totalValue = parseFloat($scope.installment.totalValue.replace(',', '.'), 2);
+        $scope.installment.installmentValue = (totalValue / $scope.installment.qtyInstallments).toFixed(2).toString();
+    }
 }
 
-InstallmentFormCtrl.$inject = ['$scope'];
+InstallmentFormCtrl.$inject = ['$scope', '$filter'];
