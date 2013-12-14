6666fa7bbcd031b3d9f93ec4b737e69d6369069d
Marcio <marcio.cruz@ispm.com.br>
2013-12-14 00:12:39 +0000

    fixing gruntfile, and html flickering

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,6 +69,7 @@ module.exports = function(grunt) {
                         'static/src/js/lib/min/highcharts.js',
                         'static/src/js/lib/min/bootstrap-select.min.js',
                         'static/src/js/lib/min/modernizr.custom.19405.js',
+                        'static/src/js/lib/min/accounting.min.js',
 
                         'static/dist/js/webapp-libs.min.tmp.js'
                     ],
