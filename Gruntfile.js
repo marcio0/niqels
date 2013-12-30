d2a5575f0f638898f72e44045c069092208af8e6
Marcio <marcio.cruz@ispm.com.br>
2013-12-30 03:38:58 +0000

    adding bootstrap tour

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,6 +70,7 @@ module.exports = function(grunt) {
                         'static/src/js/lib/min/bootstrap-select.min.js',
                         'static/src/js/lib/min/modernizr.custom.19405.js',
                         'static/src/js/lib/min/accounting.min.js',
+                        'static/src/js/lib/min/bootstrap-tour.min.js',
 
                         'static/dist/js/webapp-libs.min.tmp.js'
                     ],
