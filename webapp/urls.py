f4d70927b4aad80b438c347618f5e06b7dd7e513
Marcio <marcio.cruz@ispm.com.br>
2014-01-06 01:23:35 +0000

    cleaning

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,7 +5,6 @@ from views import WebAppView
 
 urlpatterns = patterns('',
     url(r'^partials/category-list/$', TemplateView.as_view(template_name="webapp/settings/category-list.html")),
-    url(r'^partials/reminder-list/$', TemplateView.as_view(template_name="webapp/settings/reminder-list.html")),
 
     url(r'^$', WebAppView.as_view(), name="index"),
 )
