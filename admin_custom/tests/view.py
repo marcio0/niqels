a03157bb5c5ec7389e67e1c18e7e7539aecdb213
Marcio <marcio.cruz@ispm.com.br>
2014-01-16 00:11:05 +0000

    fixing templates and validating sql

--- admin_custom/tests/view.py
+++ admin_custom/tests/view.py
@@ -1,6 +1,43 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+import mock
+from access.models import User
+from admin_custom.views import UserQueryForm, EmailInterface, validate_sql
+
+
+class SqlValidatorTest(TestCase):
+    def test_no_edit(self):
+        # no error
+        validate_sql('select * foo bla')
+
+        self.assertRaises(ValidationError, validate_sql, 'stuff update table')
+        self.assertRaises(ValidationError, validate_sql, 'blado drop table')
+        self.assertRaises(ValidationError, validate_sql, 'bla delete from table')
+        self.assertRaises(ValidationError, validate_sql, 'lero alter table')
+        self.assertRaises(ValidationError, validate_sql, 'lero insert table')
+        self.assertRaises(ValidationError, validate_sql, 'foo create table')
+        self.assertRaises(ValidationError, validate_sql, 'STUFF UPDATE TABLE')
+        self.assertRaises(ValidationError, validate_sql, 'BLADO DROP TABLE')
+        self.assertRaises(ValidationError, validate_sql, 'BLA DELETE FROM TABLE')
+        self.assertRaises(ValidationError, validate_sql, 'LERO ALTER TABLE')
+        self.assertRaises(ValidationError, validate_sql, 'LERO INSERT TABLE')
+        self.assertRaises(ValidationError, validate_sql, 'FOO CREATE TABLE')
 
 
 class EmailInterfaceViewTest(TestCase):
-    def test_ok(self):
-        pass
+    @mock.patch.object(User, 'objects')
+    def test_ok(self, objs):
+        data = {
+            'query': 'select *'
+        }
+
+        objs.raw.return_value = [1, 2]
+
+        view = EmailInterface()
+        view.request = mock.Mock()
+
+        form = UserQueryForm(data)
+        view.form_valid(form)
+
+        objs.raw.assert_called_with(data['query'])
+        self.assertEquals(form.data['result'], 2)
--- admin_custom/views.py
+++ admin_custom/views.py
@@ -1,17 +1,34 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from django.utils.encoding import force_text
 from django.views.generic.edit import FormView
+import re
+from access.models import User
+
+
+class SqlValidator(RegexValidator):
+    regex = re.compile(r'update|drop|delete|alter|insert|create', re.IGNORECASE)
+
+    def __call__(self, value):
+        if self.regex.search(force_text(value)):
+            raise ValidationError(self.message, code=self.code)
+validate_sql = SqlValidator()
 
 
 class UserQueryForm(forms.Form):
-    query = forms.CharField(required=True)
+    query = forms.CharField(required=True, validators=[SqlValidator])
     result = forms.CharField(required=False)
 
 
 class EmailInterface(FormView):
-    template_name='admin_custom/email_interface/index.html'
+    template_name='admin/email_interface/index.html'
     form_class = UserQueryForm
 
     def form_valid(self, form):
         form.data = form.data.copy()
-        form.data['result'] = 'sdasdd'
+
+        result = User.objects.raw(form.data['query'])
+        form.data['result'] = len(result)
+
         return self.render_to_response(self.get_context_data(form=form))
