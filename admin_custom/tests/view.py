27814cc70155515095d51648c643e5dd3e421270
Marcio <marcio.cruz@ispm.com.br>
2014-01-16 01:37:18 +0000

    validating, executing query, showing result

--- admin_custom/tests/view.py
+++ admin_custom/tests/view.py
@@ -1,8 +1,17 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 import mock
+import factory
 from access.models import User
 from admin_custom.views import UserQueryForm, EmailInterface, validate_sql
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = User
+    FACTORY_DJANGO_GET_OR_CREATE = ('email',)
+
+    email = factory.Sequence(lambda n: 'user{0}@example.com'.format(n))
+    password = 'password'
 
 
 class SqlValidatorTest(TestCase):
@@ -25,19 +34,40 @@ class SqlValidatorTest(TestCase):
 
 
 class EmailInterfaceViewTest(TestCase):
-    @mock.patch.object(User, 'objects')
-    def test_ok(self, objs):
+    def test_select(self):
+        UserFactory.create_batch(3)
+
         data = {
-            'query': 'select *'
+            'query': 'select * from access_user'
         }
 
-        objs.raw.return_value = [1, 2]
-
+        form = UserQueryForm(data)
         view = EmailInterface()
-        view.request = mock.Mock()
+
+        self.assertTrue(view.execute_sql(form))
+        self.assertEquals(form.data['result'], 3)
+
+    def test_database_error(self):
+        data = {
+            'query': 'asdaddsd'
+        }
 
         form = UserQueryForm(data)
-        view.form_valid(form)
+        view = EmailInterface()
 
-        objs.raw.assert_called_with(data['query'])
-        self.assertEquals(form.data['result'], 2)
+        self.assertFalse(view.execute_sql(form))
+        self.assertEquals(form.data['result'], 0)
+        self.assertTrue('sql' in form.errors)
+
+    def test_sql_validation(self):
+        terms = ['update', 'drop', 'delete', 'alter', 'insert', 'create']
+
+        for term in terms:
+            form = UserQueryForm({'query': term})
+            self.assertFalse(form.is_valid())
+
+            form = UserQueryForm({'query': term.upper()})
+            self.assertFalse(form.is_valid())
+
+        form = UserQueryForm({'query': 'select somethin from some table'})
+        self.assertTrue(form.is_valid())
--- admin_custom/views.py
+++ admin_custom/views.py
@@ -1,7 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
+from django.db import DatabaseError
+from django.db.models.query_utils import InvalidQuery
 from django.utils.encoding import force_text
+from django.utils.translation import gettext as _
 from django.views.generic.edit import FormView
 import re
 from access.models import User
@@ -9,6 +12,7 @@ from access.models import User
 
 class SqlValidator(RegexValidator):
     regex = re.compile(r'update|drop|delete|alter|insert|create', re.IGNORECASE)
+    message = _('SQL that changes data are not allowed.')
 
     def __call__(self, value):
         if self.regex.search(force_text(value)):
@@ -17,7 +21,7 @@ validate_sql = SqlValidator()
 
 
 class UserQueryForm(forms.Form):
-    query = forms.CharField(required=True, validators=[SqlValidator])
+    query = forms.CharField(required=True, validators=[validate_sql])
     result = forms.CharField(required=False)
 
 
@@ -25,10 +29,30 @@ class EmailInterface(FormView):
     template_name='admin/email_interface/index.html'
     form_class = UserQueryForm
 
-    def form_valid(self, form):
+    def execute_sql(self, form):
         form.data = form.data.copy()
 
-        result = User.objects.raw(form.data['query'])
-        form.data['result'] = len(result)
+        try:
+            result = User.objects.raw(form.data['query'])
+            form.data['result'] = len(list(result))
 
+            return True
+        except (DatabaseError, InvalidQuery):
+            form.data['result'] = 0
+            form.errors['__all__'] = 'invalid sql query'
+
+            return False
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+
+        if form.is_valid() and self.execute_sql(form):
+            print 'ok', form.errors
+            return self.form_valid(form)
+        else:
+            print 'nok', form.errors
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
         return self.render_to_response(self.get_context_data(form=form))
