71d5acb365f5d6e68ccf545fc00f12aa3790b841
Marcio <marcio.cruz@ispm.com.br>
2014-01-17 22:58:57 +0000

    finishing command

--- data/management/commands/count_active_users.py
+++ data/management/commands/count_active_users.py
@@ -1,8 +1,27 @@
+from __future__ import division
+from decimal import Decimal
 from django.core.management.base import BaseCommand, CommandError
+from datetime import datetime, timedelta
+from django.utils import timezone
+from access.models import User
+from django.db.models import Count
+from data.models import Data
 
 
 class Command(BaseCommand):
     help = u'Counts the amount of active users'
+    indicator = 'active users ratio'
 
     def handle(self, *args, **kwargs):
-        pass
+        now = timezone.now()
+        one_week_ago = now - timedelta(weeks=1)
+
+        total = User.objects.annotate(num_transactions=Count('transaction')).filter(num_transactions__gte=0).count()
+        active_users = User.objects.filter(transaction__created__gte=one_week_ago).distinct().count()
+
+        active_users_ratio = Decimal(active_users) / Decimal(total) * 100
+        active_users_ratio = '%.2f%%' % active_users_ratio
+
+        return Data.objects.create(indicator=self.indicator, value=active_users_ratio, date=now)
+
+
--- data/tests/count_active_users_command.py
+++ data/tests/count_active_users_command.py
@@ -1,10 +1,16 @@
+from decimal import Decimal
 from django.test import TestCase
+from django.utils import timezone
 import mock
 import factory
 from freezegun import freeze_time
+from datetime import timedelta, datetime
 import access.models
+from data.management.commands import count_active_users
 from expenses.models import Transaction, Category, CategoryGroup
 
+
+COMMAND_INDICATOR = 'active users ratio'
 
 class UserFactory(factory.DjangoModelFactory):
     FACTORY_FOR = access.models.User
@@ -18,14 +24,23 @@ class TransactionFactory(factory.DjangoModelFactory):
 
 
 class CommandTest(TestCase):
-
     def setUp(self):
         group = CategoryGroup.objects.create(name='group')
         self.category = Category.objects.create(name="category", group=group)
 
     @freeze_time('2010-10-01')
-    def test_simple(self):
-        user1 = UserFactory.create()
-        transactions = TransactionFactory.create_batch(2, category=self.category, user=user1, date='2010-10-01')
+    @mock.patch.object(access.models.User, 'objects')
+    def test_simple(self, user_mgr):
+        one_week_diff = timezone.now() - timedelta(weeks=1)
 
-        print user1.transactions.all()
+        user_mgr.annotate().filter().count.return_value = 10
+        user_mgr.filter().distinct().count.return_value = 5
+
+        command = count_active_users.Command()
+        data = command.handle()
+
+        self.assertEquals(data.date, timezone.now())
+        self.assertEquals(data.value, '50.00%')
+        self.assertEquals(data.indicator, COMMAND_INDICATOR)
+
+        user_mgr.filter.assert_called_with(transaction__created__gte=one_week_diff)
