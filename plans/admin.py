d89e7e49a67e75726c1da3ecfb3653a72515a3a2
Marcio <marcio.cruz@ispm.com.br>
2014-01-27 22:13:20 +0000

    finishing admin

--- plans/admin.py
+++ plans/admin.py
@@ -2,6 +2,7 @@
 
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
+from django.db.models import Count
 
 from access.admin import UserAdmin
 from plans.models import Subscription
@@ -15,3 +16,24 @@ class SubscriptionAdmin(admin.TabularInline):
     verbose_name_plural = _(u'assinaturas')
 
 UserAdmin.inlines = [SubscriptionAdmin]
+
+class SubscriptionFilter(admin.SimpleListFilter):
+    title = _(u'assinantes')
+    parameter_name = 'subscriptions'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('true', _(u'Sim')),
+            ('false', _(u'Não'))
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'true':
+            return queryset.annotate(subscription_count=Count('subscriptions')).filter(subscription_count__gt=0)
+        if self.value() == 'false':
+            return queryset.annotate(subscription_count=Count('subscriptions')).filter(subscription_count=0)
+
+UserAdmin.list_filter += (SubscriptionFilter,)
+
+
+
