c0a8c37fbc1da05da9743eb0218db453b9f215bf
Marcio <marcio.cruz@ispm.com.br>
2014-01-29 03:23:46 +0000

    starting

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.utils.translation import ugettext_lazy as _
+from django.views.generic import TemplateView
 
 from expenses import views
 
+guide_urls = patterns('',
+    url(_(r'^$'), TemplateView.as_view(template_name='guide/guide_index.html'), name='guide'),
+)
+
 urlpatterns = patterns('',
-    url(r'^$', views.LandingPage.as_view(), name='landing')
+    url(_(r'^guia/'), include(guide_urls, namespace='guide')),
+    url(r'^$', views.LandingPage.as_view(), name='landing'),
 )
