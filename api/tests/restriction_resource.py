ed38c8efb7622412c7eff6114407cd86ba65999a
Marcio <marcio.cruz@ispm.com.br>
2014-02-11 01:17:17 +0000

    basic handler, tests, etc

--- api/tests/restriction_resource.py
+++ api/tests/restriction_resource.py
@@ -1,9 +1,8 @@
-import datetime
 import factory
 from decimal import Decimal
-from time import timezone
 from access.models import User
-from expenses.models import Category
+from expenses.models import Category, CategoryGroup
+from expenses.tests.base import BaseResourceTestCase
 from restrictions.models import BaseCategoryRestriction
 
 
@@ -11,7 +10,6 @@ class CategoryFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Category
 
     name = factory.Sequence(lambda n: 'category%d' % n)
-    group_id = 1
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -27,4 +25,129 @@ class BaseCategoryRestrictionFactory(factory.DjangoModelFactory):
 
     value = Decimal("10")
     user = factory.SubFactory(UserFactory)
+
+
+class RestrictionResourceTest(BaseResourceTestCase):
+    list_url = '/api/v1/restrictions/category'
+
+    def setUp(self):
+        super(RestrictionResourceTest, self).setUp()
+
+        group = CategoryGroup.objects.create(name='group')
+
+        self.categories = CategoryFactory.create_batch(2, group=group)
+
+        self.restriction = BaseCategoryRestrictionFactory.create(user=self.user, category=self.categories[0], value=Decimal('-100'))
+        BaseCategoryRestrictionFactory.create(user=self.user, category=self.categories[1], value=Decimal('-200'))
+
+        # creating a restriction for another user
+        BaseCategoryRestrictionFactory.create(user=self.another_user, category=self.categories[1], value=Decimal('-200'))
+
+        self.detail_url = '/api/v1/restrictions/category/{0}'.format(self.restriction.id)
+
+    def test_get_list(self):
+        resp = self.api_client.get('/api/v1/restrictions/category', format='json', authentication=self.get_credentials())
+        self.assertValidJSONResponse(resp)
+
+        self.assertEqual(len(self.deserialize(resp)['objects']), 2)
+
+        self.assertEquals(self.deserialize(resp)['objects'], [
+            {
+                u'category': u'/api/v1/category/1',
+                u'id': 1,
+                u'resource_uri': u'/api/v1/restrictions/category/1',
+                u'value': u'-100'
+            },
+            {
+                u'category': u'/api/v1/category/2',
+                u'id': 2,
+                u'resource_uri': u'/api/v1/restrictions/category/2',
+                u'value': u'-200'
+            }
+        ])
+
+    def test_get_detail(self):
+        resp = self.api_client.get(self.get_detail_url(), format='json', authentication=self.get_credentials())
+
+        self.assertValidJSONResponse(resp)
+        self.assertEquals(self.deserialize(resp), {
+            u'category': u'/api/v1/category/1',
+            u'id': 1,
+            u'resource_uri': u'/api/v1/restrictions/category/1',
+            u'value': u'-100'
+        })
+
+    def test_get_detail_own_objects_only(self):
+        another_retriction = BaseCategoryRestriction.objects.filter(user_id=self.another_user.id)[0]
+        url = '/api/v1/restrictions/category/{0}'.format(another_retriction.id)
+
+        resp = self.api_client.get(url, format='json', authentication=self.get_credentials())
+        self.assertHttpUnauthorized(resp)
+
+
+    def test_post_list(self):
+        # Check how many are there first.
+        self.assertEqual(BaseCategoryRestriction.objects.filter(user=self.user).count(), 2)
+
+        # creating a new category to avoid unique clash
+        category = CategoryFactory.create(group_id=1)
+
+        post_data = {
+            'value': Decimal(-350),
+            'category': '/api/v1/category/%d' % category.id
+        }
+
+        resp = self.api_client.post(self.get_list_url(), format='json', data=post_data, authentication=self.get_credentials())
+        self.assertHttpCreated(resp)
+
+        content = self.deserialize(resp)
+
+        self.assertEquals(content, {
+            u'category': u'/api/v1/category/%d' % category.id,
+            u'id': 4,
+            u'value': u'-350',
+            u'resource_uri': u'/api/v1/restrictions/category/4'
+        })
+
+        self.assertEqual(BaseCategoryRestriction.objects.filter(user=self.user).count(), 3)
+
+    def test_post_list_uniqueness(self):
+        """
+        Testing for uniqueness of category and user combination.
+        """
+        # Check how many are there first.
+        self.assertEqual(BaseCategoryRestriction.objects.filter(user=self.user).count(), 2)
+
+        post_data = {
+            'value': Decimal(-350),
+            'category': '/api/v1/category/1'
+        }
+
+        resp = self.api_client.post(self.get_list_url(), format='json', data=post_data, authentication=self.get_credentials())
+        self.assertHttpBadRequest(resp)
+
+        self.assertEqual(BaseCategoryRestriction.objects.filter(user=self.user).count(), 2)
+
+    def test_post_send_user_no_effect(self):
+        """
+        Sending "user" property should have no effect.
+        It must use the logged-in user.
+        """
+        # Check how many are there first.
+        self.assertEqual(BaseCategoryRestriction.objects.filter(user=self.user).count(), 2)
+
+        # creating a new category to avoid unique clash
+        category = CategoryFactory.create(group_id=1)
+
+        post_data = {
+            'value': Decimal(-350),
+            'category': '/api/v1/category/1',
+            'user': 2
+        }
+
+        resp = self.api_client.post(self.get_list_url(), format='json', data=post_data, authentication=self.get_credentials())
+        self.assertHttpBadRequest(resp)
+
+        self.assertEqual(BaseCategoryRestriction.objects.filter(user=self.user).count(), 2)
+
 
---
+++ expenses/tests/base.py
@@ -0,0 +1,91 @@
+from tastypie.test import ResourceTestCase
+from access.models import User
+
+
+def skip_if_base_class(f):
+    def wrapper(self, *args, **kwargs):
+        if type(self) == BaseResourceTestCase:
+            self.skipTest('')
+        return f(self, *args, **kwargs)
+    return wrapper
+
+
+class BaseResourceTestCase(ResourceTestCase):
+
+    def setUp(self):
+        super(BaseResourceTestCase, self).setUp()
+
+        self.email = 'user@example.com'
+        self.password = 'password'
+        self.user = User.objects.create_user(self.email, self.password)
+
+        self.another_user = User.objects.create_user('another@example.com', 'password')
+
+    def get_list_url(self):
+        return self.list_url
+
+    def get_detail_url(self):
+        return self.detail_url
+
+    def get_credentials(self):
+        """
+        Get the credentials for basic http authentication.
+        """
+        return self.create_basic(username=self.email, password=self.password)
+
+    @skip_if_base_class
+    def test_basic_auth_ok(self):
+        """
+        Testing auth with basic HTTP authentication.
+        """
+        resp = self.api_client.get(self.get_list_url(), format='json', authentication=self.get_credentials())
+        self.assertValidJSONResponse(resp)
+
+    @skip_if_base_class
+    def test_session_auth_ok(self):
+        """
+        Testing auth with django's session authentication.
+        User is alread logged in, so there's no need for auth data on requisition.
+        """
+        self.assertTrue(self.api_client.client.login(email=self.email, password=self.password))
+        resp = self.api_client.get(self.get_list_url(), format='json')
+        self.assertValidJSONResponse(resp)
+
+    @skip_if_base_class
+    def test_get_list_unauthorized(self):
+        """
+        Must be authenticated to send a GET request to a list endpoint.
+        """
+        self.assertHttpUnauthorized(self.api_client.get(self.get_list_url(), format='json'))
+
+    @skip_if_base_class
+    def test_get_list(self):
+        raise NotImplemented()
+
+
+    @skip_if_base_class
+    def test_get_detail_unauthorized(self):
+        """
+        Must be authenticated to send GET request to a detail endpoint.
+        """
+        self.assertHttpUnauthorized(self.api_client.get(self.get_detail_url(), format='json'))
+
+    @skip_if_base_class
+    def test_get_detail(self):
+        raise NotImplemented()
+
+    @skip_if_base_class
+    def test_get_detail_own_objects_only(self):
+        raise NotImplemented()
+
+
+    @skip_if_base_class
+    def test_post_list_unauthorized(self):
+        """
+        Must be authenticated to POST a list endpoint.
+        """
+        self.assertHttpUnauthorized(self.api_client.post(self.get_list_url(), format='json'))
+
+    @skip_if_base_class
+    def test_post_list(self):
+        raise NotImplemented()
