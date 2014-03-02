9e726d0ab7a8917223d9ad90c87c703aa7c51d2d
Marcio <marcio.cruz@ispm.com.br>
2014-03-02 04:14:43 +0000

    plan info

--- plans/urls.py
+++ plans/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.views.generic.base import TemplateView, View
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='plans/plans-index.html'), name='index')
+    url(r'^$', TemplateView.as_view(template_name='plans/plans-index.html'), name='index'),
+    url(r'^cancel$', TemplateView.as_view(template_name='plans/cancel.html'), name='cancel')
 )
