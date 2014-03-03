4f47b802c61a450b458607e8906cb7b6895c4b92
Marcio <marcio.cruz@ispm.com.br>
2014-03-03 03:08:17 +0000

    sending email

--- plans/views.py
+++ plans/views.py
@@ -1,3 +1,6 @@
+# encoding: utf-8
+from django.core.mail import mail_admins
+
 from django.forms import Form
 from django.utils import timezone
 from django.views.generic import FormView
@@ -9,8 +12,22 @@ class CancelSubscriptionView(FormView):
     form_class = Form
     success_url = '/'
 
+    # not unit tested
     def _send_cancel_notifiation(self, user):
-        pass
+        subscription = user.has_subscription()[0]
+
+        subject = 'Cancelamento de assinatura'
+        message = u"Usuário: %(user_email)s \n" \
+                  u"Início da assinatura: %(subs_start_date)s \n" \
+                  u"Detalhe da assinatura: %(subs_desc)s" \
+                  u"\n" \
+                  u"Favor conferir o status da assinatura no admin panel e entrar em contato com o usuário" % {
+            'user_email': user.email,
+            'subs_start_date': subscription.start_date,
+            'subs_desc': subscription.name
+        }
+
+        mail_admins(subject, message)
 
     def form_valid(self, form):
         user = self.request.user
