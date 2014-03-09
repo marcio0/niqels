277a1f068fbae2c7ff914cc2d9d960ffc206d640
Marcio <marcio.cruz@ispm.com.br>
2014-03-09 04:22:58 +0000

    starting feedback form

---
+++ static/src/js/app/controllers/feedback-form-ctrl.js
@@ -0,0 +1,10 @@
+function FeedbackFormCtrl ($scope, $rootScope) {
+    'use strict';
+
+    $scope.sendFeedback = function () {
+        debugger;
+        $scope;
+    };
+}
+
+FeedbackFormCtrl.$inject = ['$scope', '$rootScope'];
