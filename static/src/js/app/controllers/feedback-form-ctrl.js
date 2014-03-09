59922e2e28bb1db5ada837e8aa250acd27a9df6a
Marcio <marcio.cruz@ispm.com.br>
2014-03-09 04:24:21 +0000

    comments'

--- static/src/js/app/controllers/feedback-form-ctrl.js
+++ static/src/js/app/controllers/feedback-form-ctrl.js
@@ -1,10 +1,13 @@
-function FeedbackFormCtrl ($scope, $rootScope) {
+function FeedbackFormCtrl ($scope, $rootScope, $http) {
     'use strict';
 
     $scope.sendFeedback = function () {
+        // use $http to send the message to the backend
+        // on the backend get the user email from the logged user
+        // treat as a ajax request
         debugger;
         $scope;
     };
 }
 
-FeedbackFormCtrl.$inject = ['$scope', '$rootScope'];
+FeedbackFormCtrl.$inject = ['$scope', '$rootScope', '$http'];
